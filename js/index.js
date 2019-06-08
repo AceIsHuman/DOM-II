fe54c58f9eac20c7ccb987178b8da2636a094ef2
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-06-08 12:41:08 -0500

    added focus and blur to input fields

--- js/index.js
+++ js/index.js
@@ -3,10 +3,6 @@ function floatUp() {
     this.classList.toggle('float-up');
 }
 
-function redFontColor() {
-    this.style.color = 'red';
-}
-
 const headers = document.querySelectorAll('h1, h2, h3, h4');
 headers.forEach(elm => {
     elm.addEventListener('mouseover', floatUp);
@@ -15,9 +11,9 @@ headers.forEach(elm => {
 
 
 const input = document.querySelectorAll('input');
-input.forEach(elm => {
-    elm.addEventListener('keydown', () => console.log('KEYS WERE PRESSED!'));
-})
+input.forEach(elm => elm.addEventListener('keydown', () => console.log('KEYS WERE PRESSED!')));
+input.forEach(elm => elm.addEventListener('focus', function(){this.style.background = 'rgba(23, 162, 184, 0.5)'}));
+input.forEach(elm => elm.addEventListener('blur', function(){this.style.background = ''}));
 
 const textContent = document.querySelectorAll('p');
 textContent.forEach(elm => elm.addEventListener('drag', function(){this.style.color = 'red'}));
