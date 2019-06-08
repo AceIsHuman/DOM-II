1b4731bd9e5136f67c54bd02b166f125c185e7b2
AceIsHuman <47653241+AceIsHuman@users.noreply.github.com>
2019-06-08 12:36:09 -0500

    added a couple of event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+function floatUp() {
+    this.classList.toggle('float-up');
+}
+
+function redFontColor() {
+    this.style.color = 'red';
+}
+
+const headers = document.querySelectorAll('h1, h2, h3, h4');
+headers.forEach(elm => {
+    elm.addEventListener('mouseover', floatUp);
+    elm.addEventListener('mouseleave', floatUp);
+});
+
+
+const input = document.querySelectorAll('input');
+input.forEach(elm => {
+    elm.addEventListener('keydown', () => console.log('KEYS WERE PRESSED!'));
+})
+
+const textContent = document.querySelectorAll('p');
+textContent.forEach(elm => elm.addEventListener('drag', function(){this.style.color = 'red'}));
+textContent.forEach(elm => elm.addEventListener('dragend', function(){this.style.color = 'black'}));
